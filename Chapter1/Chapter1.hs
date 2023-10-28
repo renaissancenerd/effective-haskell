30183cd80f3a7bc37b2bb52253dac5d471740056
renaissancenerd <vatsallimbachiya@gmail.com>
2023-10-28 16:26:40 +0200

    composing functions

---
+++ Chapter1/Chapter1.hs
@@ -0,0 +1,7 @@
+module Chapter1 where
+    addOne num = num + 1
+    timesTwo num = num * 2
+    squared num = num * num
+    minusFive num = num - 5
+
+    findResult num = minusFive(squared(timesTwo(addOne num)))
